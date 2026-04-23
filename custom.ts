98089e1bda0aa83e4173636d6011c29d143e1d7e
akahoshi1421 <akaho1421@gmail.com>
2026-04-23 23:33:37 +0900

    chore

--- custom.ts
+++ custom.ts
@@ -35,12 +35,12 @@ namespace hiragana_agent {
 
     //% blockId=hiragana_agent_13
     //% block="もし $hoge なら"
+    //% hoge.shadow=hiragana_shingi
     //% handlerStatement=true
-    //% hoge.defl=true
     //% topblock=false
-    export function moshi(hoge: Bool, handler: () => void): void {
-        if(hoge === Bool.Shin) handler();
-    }    
+    export function moshi(hoge: boolean, handler: () => void): void {
+        if (hoge) handler();
+    } 
 
     //% blockId=hiragana_agent_11
     //% block="はじめに"
